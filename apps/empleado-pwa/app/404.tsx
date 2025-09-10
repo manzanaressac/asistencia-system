b67c82faaf5ff9650e0b958dbd7fef75de634088
manzanaressac <manzanaresenlinea@gmail.com>
2025-09-10 17:51:15 -0500

    actualización 1.0.5

--- apps/empleado-pwa/app/404.tsx
+++ apps/empleado-pwa/app/404.tsx
@@ -1,24 +1,13 @@
 'use client';
-
 import Link from 'next/link';
-import React from 'react';
 
 export default function Custom404() {
   return (
     <div style={{ textAlign: 'center', marginTop: '5rem' }}>
-      <h1 style={{ fontSize: '4rem' }}>404</h1>
+      <h1>404</h1>
       <h2>Página no encontrada</h2>
-      <p>La página que buscas no existe.</p>
       <Link href="/">
-        <button
-          style={{
-            padding: '0.5rem 1rem',
-            marginTop: '1rem',
-            cursor: 'pointer',
-          }}
-        >
-          Volver al inicio
-        </button>
+        <button>Volver al inicio</button>
       </Link>
     </div>
   );
